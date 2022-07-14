8d9e7144904dffc9ede051c8af71a58282b4e9aa
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-14 14:59:13 -0300

    feat: basic commands in MySQL

---
+++ basic/data.sql
@@ -0,0 +1,10 @@
+CREATE TABLE pessoas (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  nome VARCHAR(20) NOT NULL,
+  nascimento DATE
+);
+
+INSERT INTO pessoas (nome, nascimento) VALUES ('João', '1980-01-01');
+INSERT INTO pessoas (nome, nascimento) VALUES ('Gabriel', '1998-11-11');
+INSERT INTO pessoas (nome, nascimento) VALUES ('Euclides', '1960-05-20');
+
