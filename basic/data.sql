790163f59dcd1e557315e2caa1514a50fa56b8c4
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-15 17:10:58 -0300

    feat: add new atribute genero

--- basic/data.sql
+++ basic/data.sql
@@ -1,7 +1,8 @@
 CREATE TABLE pessoas (
   id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   nome VARCHAR(20) NOT NULL,
-  nascimento DATE
+  nascimento DATE,
+  genero VARCHAR(1),
 );
 
 INSERT INTO pessoas (nome, nascimento) VALUES ('João', '1980-01-01');
